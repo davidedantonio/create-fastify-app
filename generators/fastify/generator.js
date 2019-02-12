d1a15a9455a12885fb3afed3623244f65299b58c
davide <davide.dantonio@webeetle.com>
2019-02-12 17:38:40 +0100

    fix

--- generators/fastify/generator.js
+++ generators/fastify/generator.js
@@ -1,18 +1,17 @@
 'use strict'
 
-const swagger = require('../../lib/swagger')
+const beautify = require('js-beautify').js
+const fs = require('fs')
 const Handlebars = require('../../lib/handlebars')
 const path = require('path')
-const fs = require('fs')
-const beautify = require('js-beautify').js
-const _ = require('lodash')
+const swagger = require('../../lib/swagger')
 
 function createTemplate (template, data) {
   const generateTemplate = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'templates', template), 'utf8'))
   return generateTemplate(data)
 }
 
-async function generatePlugin (filePath, projectFolder)  {
+async function generatePlugin (filePath, projectFolder) {
   let fileContent = await swagger(path.resolve(__dirname, filePath))
   let content = createTemplate(path.join('plugins', 'swagger.hbs'), fileContent)
   fs.writeFileSync(path.join(projectFolder, 'app', 'plugins', 'swagger.js'), beautify(content, { indent_size: 2, space_in_empty_paren: true }), 'utf8')
@@ -20,7 +19,7 @@ async function generatePlugin (filePath, projectFolder)  {
 
 async function generateServices (filePath, projectFolder) {
   let fileContent = await swagger(path.resolve(__dirname, filePath))
-  const files = {};
+  const files = {}
 
   for (let pathName in fileContent.paths) {
     let endpointName = fileContent.paths[pathName].endpointName
@@ -30,13 +29,12 @@ async function generateServices (filePath, projectFolder) {
 
     let realPathName = pathName.replace(/}/g, '').replace(/{/g, ':')
     files[endpointName].push({
-      endpointName: (realPathName.substring(endpointName.length+1) || '/').replace(/}/g, '').replace(/{/g, ':'),
+      endpointName: (realPathName.substring(endpointName.length + 1) || '/').replace(/}/g, '').replace(/{/g, ':'),
       path: fileContent.paths[pathName]
     })
   }
 
   const servicesPath = path.join(projectFolder, 'app', 'services')
-  const testPath = path.join(projectFolder, 'test', 'services')
 
   for (let prefix in files) {
     let serviceContent = createTemplate(path.join('services', 'service.hbs'), { prefix: prefix, data: files[prefix] })
