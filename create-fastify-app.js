5b0e3cf71a37773852559faf6d57c43e683529f9
davidedantonio <davide.dantonio@webeetle.com>
2019-02-20 08:27:00 +0100

    addded mongodb cli

--- create-fastify-app.js
+++ create-fastify-app.js
@@ -4,11 +4,13 @@
 
 const appGenerator = require('./generators/fastify')
 const serviceGenerator = require('./generators/service')
+const mongoGenerator = require('./generators/mongodb')
 const commist = require('commist')()
 const utils = require('./lib/utils')
 
 commist.register('generate:project', appGenerator.cli)
 commist.register('generate:service', serviceGenerator.cli)
+commist.register('add:mongo', mongoGenerator.cli)
 
 const res = commist.parse(process.argv.splice(2))
 
