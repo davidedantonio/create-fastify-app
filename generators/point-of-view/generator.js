e10d169e4ee20940f8789f28936be679ea4f5cb4
davidedantonio <davide.dantonio@webeetle.com>
2019-02-22 17:02:07 +0100

    point of view

--- generators/point-of-view/generator.js
+++ generators/point-of-view/generator.js
@@ -13,6 +13,7 @@ function createTemplate (template, data) {
 
 async function generatePlugin (pluginPath, answers) {
   const rootProjectPath = getAbsolutePath(path.join(pluginPath, '..', '..'))
+  const servicesPath = getAbsolutePath(path.join(pluginPath, '..', 'services'))
 
   if (fs.existsSync(path.join(pluginPath, 'pov.js'))) {
     throw new Error('Point-of-view plugin already configured')
@@ -20,6 +21,39 @@ async function generatePlugin (pluginPath, answers) {
 
   let content = createTemplate('pov.hbs', answers)
   fs.writeFileSync(path.join(pluginPath, 'pov.js'), content, 'utf8')
+
+  fs.mkdirSync(path.join(pluginPath, '..', 'templates'))
+
+  let fileExtension
+  switch (answers.engine) {
+    case 'handlebars':
+      fileExtension = 'hbs'
+      break
+    case 'ejs-mate':
+    case 'ejs':
+      fileExtension = 'ejs'
+      break
+    case 'nunjucks':
+      fileExtension = 'njk'
+      break
+    case 'pug':
+      fileExtension = 'pug'
+      break
+    case 'mustache':
+      fileExtension = 'mustache'
+      break
+    case 'marko':
+      fileExtension = 'marko'
+      break
+  }
+
+  const copyFrom = path.join(__dirname, 'templates', 'examples', `example.${fileExtension}`)
+  const copyTo = path.join(pluginPath, '..', 'templates', `example.${fileExtension}`)
+  fs.copyFileSync(copyFrom, copyTo)
+
+  fs.mkdirSync(path.join(servicesPath, 'template-example'))
+  let service = createTemplate('service.hbs')
+  fs.writeFileSync(path.join(servicesPath, 'template-example', 'index.js'), service, 'utf8')
 
   try {
     const pkg = readPkg(rootProjectPath)
