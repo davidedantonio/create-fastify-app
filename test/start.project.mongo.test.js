c547d003fa368a81b9d8bc5924d1467b07a63d95
davidedantonio <davide.dantonio@webeetle.com>
2019-02-24 20:00:22 +0100

    mongodb plugin test

---
+++ test/start.project.mongo.test.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const t = require('tap')
+const { test } = t
+const server = require('./workdir/server')
+const path = require('path')
+const { run, ENTER } = require('./helpers/inputify')
+const { existsSync } = require('fs')
+
+test('add mongo plugin and start server', (t) => {
+  t.plan(6)
+
+  run(
+    ['create-fastify-app.js', 'add:mongo', '-d', `./test/workdir`],
+    [
+      `localhost${ENTER}`,
+      `27017${ENTER}`,
+      `test${ENTER}`,
+      `${ENTER}`,
+      `${ENTER}`
+    ]
+  ).then( _ => {
+    server.start(['-f', path.join(__dirname, 'workdir', 'app', 'app.js')], function (err, fastify) {
+      t.error(err)
+      t.ok(fastify.mongo)
+      t.ok(fastify.mongo.client)
+      t.ok(fastify.mongo.ObjectId)
+      t.ok(fastify.mongo.db)
+
+      fastify.close(() => {
+        t.pass('server closed')
+      })
+    })
+  })
+})
+
+test('check mongo plugin files', (t) => {
+  t.plan(3)
+
+  t.ok(existsSync(path.join(__dirname, 'workdir', 'app')))
+  t.ok(existsSync(path.join(__dirname, 'workdir', 'app', 'plugins')))
+  t.ok(existsSync(path.join(__dirname, 'workdir', 'app', 'plugins', 'mongo.db.js')))
+})
