16d9d148beaee25d32d6e874dc6ece66355b39a7
davidedantonio <davide.dantonio@webeetle.com>
2019-02-25 14:08:05 +0100

    added -d parameter

--- generators/service/index.js
+++ generators/service/index.js
@@ -29,21 +29,17 @@ function generate (args, cb) {
     return showHelp()
   }
 
-  if (opts._.length !== 1) {
-    log('error', 'Missing required <service-name> parameter\n')
-    return showHelp()
-  }
-
   const dir = opts.directory || process.cwd()
-  const serviceName = _.camelCase(opts._[0])
 
-  isValidFastifyProject(dir, serviceName, async err => {
+  isValidFastifyProject(dir, null, async err => {
     if (err) {
       return cb(err)
     }
 
     const prompt = inquirer.createPromptModule()
-    const answers = await prompt([{
+    const answers = await prompt([
+    { type: 'input', name: 'serviceName', message: 'Service Name', default: 'serviceName' },
+    {
       type: 'checkbox',
       name: 'methods',
       message: 'What methods do you want to generate',
@@ -77,7 +73,9 @@ function generate (args, cb) {
       }
     },
     { type: 'input', name: 'autoPrefix', message: 'Route Prefix', default: '/api' }
-    ])
+  ])
+
+    const serviceName = _.camelCase(answers.serviceName)
 
     let data = Object.assign(answers, {
       serviceName: serviceName
