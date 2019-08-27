f65bb8f85e6a20c87fda0124112d24c91b52d3d0
davide <davide.dantonio@webeetle.com>
2019-08-27 17:56:21 +0200

    started pov command

---
+++ generators/point-of-view/index.js
@@ -0,0 +1,120 @@
+'use strict'
+
+const fs = require('fs')
+const { promisify } = require('util')
+const log = require('../../lib/log')
+const path = require('path')
+const inquirer = require('inquirer')
+const Handlebars = require('./../../lib/handlebars')
+const _ = require('lodash')
+const chalk = require('chalk')
+const { parseArgs, isValidFastifyProject } = require('../../lib/utils')
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const mkdir = promisify(fs.mkdir)
+
+function stop (err) {
+  if (err) {
+    log('error', err)
+    process.exit(1)
+  }
+  process.exit(0)
+}
+
+async function showHelp () {
+  try {
+    const file = await readFile(path.join(__dirname, '..', '..', 'help', 'usage.txt'), 'utf8')
+    log('info', file)
+  } catch (e) {
+    return module.exports.stop(e)
+  }
+  return module.exports.stop()
+}
+
+async function createTemplate (template, data) {
+  let file
+  try {
+    file = await readFile(path.join(__dirname, 'templates', template), 'utf8')
+  } catch (e) {
+    module.exports.stop(e)
+  }
+
+  const serviceTemplate = Handlebars.compile(file)
+  return serviceTemplate(data)
+}
+
+async function generate (args, cb) {
+  let opts = parseArgs(args)
+  if (opts.help) {
+    return showHelp()
+  }
+
+  const dir = opts.directory || process.cwd()
+
+  try {
+    await isValidFastifyProject(dir, null)
+  } catch (e) {
+    return cb(e)
+  }
+
+  const prompt = inquirer.createPromptModule()
+  const answers = await prompt([
+    { type: 'input', name: 'views_directory', message: 'Views directory name?', default: 'views' },
+    {
+      name: 'engine',
+      message: 'What engine do you want to use',
+      choices: [
+        'ejs',
+        'ejs-mate',
+        'nunjucks',
+        'pug',
+        'handlebars',
+        'marko',
+        'mustache',
+        'art-template'
+      ],
+      validate: answers => {
+        if (answers.length < 1) {
+          return 'You must choose an engine.'
+        }
+        return true
+      }
+    }
+  ])
+
+  const viewDirectrory = _.camelCase(answers.views_directory)
+
+  let data = Object.assign(answers, {
+    serviceName: serviceName
+  })
+
+  const servicesPath = path.join(dir, 'src', 'services')
+  const testPath = path.join(dir, 'test', 'services')
+
+  try {
+    await mkdir(path.join(servicesPath, serviceName))
+
+    let content = await createTemplate('service.hbs', data)
+    await writeFile(path.join(servicesPath, serviceName, 'index.js'), content, 'utf8')
+    log('success', `File ${chalk.bold(path.join(servicesPath, serviceName, 'index.js'))} generated successfully with ${data.methods.join(', ')} methods`)
+
+    content = await createTemplate('README.md', data)
+    await writeFile(path.join(servicesPath, serviceName, 'README.md'), content, 'utf8')
+    log('success', `File ${chalk.bold(path.join(servicesPath, serviceName, 'README.md'))} generated successfully`)
+
+    content = await createTemplate('service.test.hbs', data)
+    await writeFile(path.join(testPath, `${serviceName}.test.js`), content, 'utf8')
+    log('success', `File ${chalk.bold(path.join(testPath, `${serviceName}.test.js`))} generated successfully`)
+  } catch (e) {
+    return cb(e)
+  }
+
+  log('success', 'point-of-view enabled successfully')
+  cb()
+}
+
+function cli (args) {
+  generate(args, module.exports.stop)
+}
+
+module.exports = { cli, stop, generate }
